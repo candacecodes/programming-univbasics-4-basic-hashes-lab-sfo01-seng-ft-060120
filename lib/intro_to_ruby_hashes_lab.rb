6d19d26d5829e4b926d7a9474aee0a29f79677e2
eager-class-5814 <candacehchoe@gmail.com>
2020-05-18 23:56:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,17 @@
 def new_hash
-  # return an empty hash
+  new_has = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my hash = {key: object}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {name: "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
+ id_hash_generator = 
   # return a hash with a key :id assigned to the provided number
 end
